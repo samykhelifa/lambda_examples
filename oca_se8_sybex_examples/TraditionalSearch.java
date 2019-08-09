8b9495ac407af9be8c0730a71efc75326689c0ce
samy <39716963+samykhelifa@users.noreply.github.com>
2019-08-09 22:10:13 +0200

    Create TraditionalSearch.java

---
+++ oca_se8_sybex_examples/TraditionalSearch.java
@@ -0,0 +1,20 @@
+public class TraditionalSearch {
+
+   public static void main(String[] args) {
+        List<Animal> animals = new ArrayList<Animal>();  // list of animals
+         animals.add(new Animal("fish", false, true));
+         animals.add(new Animal("kangaroo", true, false));
+         animals.add(new Animal("rabbit", true, false));
+         animals.add(new Animal("turtle", false, true));
+        print(animals, new CheckIfHopper());      // pass class that does check
+    }
+
+   private static void print(List<Animal> animals, CheckTrait checker) {
+      for (Animal animal : animals) {
+          if (checker.test(animal))               // the general check
+              System.out.print(animal + " ");
+              }
+          System.out.println();
+     }
+
+ }
