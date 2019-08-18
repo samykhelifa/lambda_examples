4849096e5967918d57b01fde6cff2da1fa6bbbbd
samy <39716963+samykhelifa@users.noreply.github.com>
2019-08-18 22:01:42 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
 
-
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
   public static void main(final String[] args) {
-    
-
-    System.out.println(" ");
+         System.out.println(" ");
   }
 }
